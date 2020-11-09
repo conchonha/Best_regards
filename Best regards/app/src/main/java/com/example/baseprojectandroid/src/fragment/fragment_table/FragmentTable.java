56b8859ea1736ceb35558a53d4d0426ba2bf215d
sangthaiba <kzumi2110>
2020-11-09 14:34:07 +0700

    firrt commit

---
+++ Best regards/app/src/main/java/com/example/baseprojectandroid/src/fragment/fragment_table/FragmentTable.java
@@ -0,0 +1,22 @@
+package com.example.baseprojectandroid.src.fragment.fragment_table;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.baseprojectandroid.R;
+
+public class FragmentTable extends Fragment {
+    private View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_table,container,false);
+        return view;
+    }
+}
--- Best regards/app/src/main/java/com/example/baseprojectandroid/src/page/splash_activity/SplashActivity.java
+++ Best regards/app/src/main/java/com/example/baseprojectandroid/src/page/splash_activity/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.example.baseprojectandroid.src.page;
+package com.example.baseprojectandroid.src.page.splash_activity;
 
 import android.os.Bundle;
 
@@ -7,12 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.baseprojectandroid.R;
 
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 
     }
 }
