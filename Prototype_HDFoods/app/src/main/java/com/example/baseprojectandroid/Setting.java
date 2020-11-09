e937c24c100f4f89b9300d207729c91db0e1867c
sangthaiba <kzumi2110>
2020-11-09 09:18:52 +0700

    new project

---
+++ Prototype_HDFoods/app/src/main/java/com/example/baseprojectandroid/Setting.java
@@ -0,0 +1,4 @@
+package com.example.baseprojectandroid;
+
+public class Setting {
+}
---
+++ Prototype_HDFoods/app/src/main/java/com/example/baseprojectandroid/src/page/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.baseprojectandroid.src.page;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.baseprojectandroid.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
